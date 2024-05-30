fb5fcddeafb89510bd9a7120e70e31c43cebf87f
Esteban Angee <esteban.angee@gmail.com>
2024-05-30 16:44:02 +0000

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+module "label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  namespace  = "eg"
+  stage      = "prod"
+  name       = "bastion"
+  attributes = ["public"]
+  delimiter  = "-"
+
+  tags = {
+    "BusinessUnit" = "XYZ",
+    "Snapshot"     = "true"
+  }
+}
